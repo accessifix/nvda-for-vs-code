892e8f16fe98f09e0607cff6b1f76488e75464a3
Christopher Toth <q.alpha@gmail.com>
2021-07-12 18:15:13 -0600

    Declare compatibility with NVDA 2021.1 and bump version

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("NVDA add-on for Microsoft Visual Studio Code improving focus management, editor behavior, and code completion."),
 	# version
-	"addon_version" : "20.2.0",
+	"addon_version" : "21.7.0",
 	# Author(s)
 	"addon_author" : u"Pawel Urbanski <support@accessifix.com>",
 	# URL for the add-on documentation support
@@ -29,7 +29,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0")
 	"addon_minimumNVDAVersion" : "2020.4",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2020.4",
+	"addon_lastTestedNVDAVersion" : "2021.1",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : "stable",
 }
