3ecd3030648bb688b1d4bf73ea60c3547f40e255
pawelurbanski <pawel@pawelurbanski.com>
2022-06-24 17:56:27 +0200

    Compatibility update with code simplification.

--- buildVars.py
+++ buildVars.py
@@ -14,12 +14,12 @@ addon_info = {
 	"addon_name" : "nvda-for-vs-code",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("NVDA-For-VS-Code"),
+	"addon_summary" : _("NVDA For VS Code"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("NVDA add-on for Microsoft Visual Studio Code improving focus management, editor behavior, and code completion."),
 	# version
-	"addon_version" : "21.0.0",
+	"addon_version" : "2022.1.0",
 	# Author(s)
 	"addon_author" : u"Pawel Urbanski <support@accessifix.com>",
 	# URL for the add-on documentation support
@@ -27,9 +27,9 @@ addon_info = {
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0")
-	"addon_minimumNVDAVersion" : "2020.3",
+	"addon_minimumNVDAVersion" : "2021.2",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2021.1",
+	"addon_lastTestedNVDAVersion" : "2022.1",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : "stable",
 }
